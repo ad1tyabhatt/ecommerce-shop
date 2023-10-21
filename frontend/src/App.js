bbbfe70943254002a221a467fb2c26c9bc4042fc
Aditya Bhatt <adityabhatt789@gmail.com>
2023-10-21 12:35:48 +0530

    Added images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './components/Header'
+import { Container } from 'react-bootstrap'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
